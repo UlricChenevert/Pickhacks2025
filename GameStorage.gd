fa82f807395c8d3a9df12375d083a013847fd65e
Elyza Syarin <nem9gt@mst.edu>
2025-03-01 13:49:22 -0600

    Script folder for the game is here
    Updated gamestorage with getting rid and adding in inventory item

--- GameStorage.gd
+++ GameStorage.gd
@@ -1,11 +1,32 @@
 extends Node
 
+# storage of an economy
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var player_coins: int = 1000
+var player_inventory: Array = []
+var asset_inventory: Dictionary = {}
 
+# function to sell items (add coins to your bank)
+func sell_items(worker: Node, item_name: String, price: int) -> void:
+	if item_name in player_inventory:
+		player_inventory.erase(item_name)
+		player_coins += price
+		print("Sold", item_name, price, "coins. Total coins: ", player_coins)
+	else:
+		print("Item was not sold.")
+		
+# function to buy an item (deducts coinS)
+func buy_item(worker: Node, item_name: String, price: int) -> void:
+	if player_coins >= price:
+		player_coins -= price
+		player_inventory.append(item_name)
+		print("Brought", item_name, "for", price, "coins. Remainiing coins: ", player_coins)
+	else:
+		print("Item was not purchased.")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# function to remove / add in inventory
+func has_item(item: String) -> bool:
+		return asset_inventory.get(item, 0) > 0
+
+func add_to_inventory(item: String) -> void:
+		asset_inventory[item] = asset_inventory.get(item, 0) + 1
